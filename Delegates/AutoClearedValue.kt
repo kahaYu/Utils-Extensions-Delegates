c0d271c08476cebf6dc3b25908ce31bad28f4b9c
Yura Kolesnikov <yurikolesnicov@gmail.com>
2021-11-19 10:41:59 +0300

    Create AutoClearedValue.kt

---
+++ Delegates/AutoClearedValue.kt
@@ -0,0 +1,33 @@
+// Makes value auto cleared in onDestoy method. 
+// It's useful for binding objects of DataBinding and ViewBinding. Used in Fragments, because there is no need to null binding object in Activity. 
+class AutoClearedValue<T : Any>(val fragment: Fragment) : ReadWriteProperty<Fragment, T> {
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object: DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                    viewLifecycleOwner?.lifecycle?.addObserver(object: DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            _value = null
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        return _value ?: throw IllegalStateException(
+            "should never call auto-cleared-value get when it might not be available"
+        )
+    }
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        _value = value
+    }
+}
+
+// Example of initialization in Fragment:
+//class NavBarFragment : Fragment() {
+    //private var binding by AutoClearedValue<FragmentNavBarBinding>(this)
